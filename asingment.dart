f4e271520e30c6cb21f3de0e164dc6d43028c5a8
mariful91 <mdmarifulislamashik@gmail.com>
2024-07-09 22:44:51 +0600

    9 tarik update

---
+++ asingment.dart
@@ -0,0 +1,6 @@
+void main(){
+
+print("mari ful");
+
+
+}
---
+++ sanjid.dart
@@ -0,0 +1,15 @@
+void main() {
+  int age = 12;
+
+  if (age <= 12) {
+    print("childhood");
+  } else if (age <= 16) {
+    print("childhood");
+  } else if (age <= 21) {
+    print("adolescence");
+  } else {
+    print("adolescence");
+  }
+ 
+ 
+}
