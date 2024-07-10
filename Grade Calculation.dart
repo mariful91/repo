49b9814a826612b37b55cdcc7ca671dec57f94a2
mariful91 <mdmarifulislamashik@gmail.com>
2024-07-10 22:46:51 +0600

    ----

---
+++ Grade Calculation.dart
@@ -0,0 +1,15 @@
+void main() {
+  int marks = 75;
+
+  if (marks >= 85) {
+    print("Grade: A");
+  } else if (marks >= 80) {
+    print("Grade: B");
+  } else if (marks >= 65) {
+    print("Grade: C");
+  } else if (marks >= 53) {
+    print("Grade: D");
+  } else {
+    print("Grade: F");
+  }
+}
